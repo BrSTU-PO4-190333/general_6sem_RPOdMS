898998e39d7c33d619f2cfb4453b309c0369d2c7
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2022-04-30 00:00:32 +0300

    Yakovchik_Ilya : Added 9 lab

---
+++ Yakovchik_Ilya/lab9/sources/app/src/main/java/com/example/lab99/gallery.java
@@ -0,0 +1,48 @@
+package com.example.lab99;
+
+//import android.os.Bundle;
+//import android.view.View;
+//import android.widget.ImageView;
+//
+//public class gallery {
+//}
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class gallery extends AppCompatActivity {
+
+    private Integer[] pictures = {R.drawable.one, R.drawable.two, R.drawable.three};
+    private int curIndex = 0;
+    ImageView iv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery);
+        iv = (ImageView)findViewById(R.id.imageView1);
+        iv.setImageResource(pictures[curIndex]);
+    }
+    public void previous(View view){
+        if(curIndex == 0){
+            curIndex = pictures.length - 1;
+            iv.setImageResource(pictures[curIndex]);
+        }
+        else{
+            curIndex--;
+            iv.setImageResource(pictures[curIndex]);
+        }
+    }
+    public void next(View view){
+        if(curIndex == pictures.length-1){
+            curIndex = 0;
+            iv.setImageResource(pictures[curIndex]);
+        }
+        else{
+            curIndex++;
+            iv.setImageResource(pictures[curIndex]);
+        }
+    }
+}
