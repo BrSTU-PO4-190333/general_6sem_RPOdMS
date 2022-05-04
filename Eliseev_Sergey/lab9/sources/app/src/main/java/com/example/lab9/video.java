08908165562b79584df02effa38379f8ede3dc46
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2022-05-05 02:43:58 +0300

    Eliseev_Sergey : Added 9 lab

---
+++ Eliseev_Sergey/lab9/sources/app/src/main/java/com/example/lab9/video.java
@@ -0,0 +1,33 @@
+package com.example.lab9;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.VideoView;
+
+public class video extends AppCompatActivity {
+
+    VideoView videoPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+
+        videoPlayer = (VideoView) findViewById(R.id.videoPlayer);
+        Uri myVIdeoUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.videoplayback);
+        videoPlayer.setVideoURI(myVIdeoUri);
+    }
+    public void play(View view){
+        videoPlayer.start();
+    }
+    public void pause(View view){
+        videoPlayer.pause();
+    }
+    public void stop(View view){
+        videoPlayer.stopPlayback();
+        videoPlayer.resume();
+    }
+}
