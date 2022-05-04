f06abdb64d132704d1618463144a3a7583474f7f
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2022-05-05 02:49:58 +0300

    Tupik_Denis : Made 1 lab

---
+++ Tupik_Denis/lab1/sources/app/src/main/java/com/example/lab01/MainActivity.java
@@ -0,0 +1,15 @@
+//laba01
+package com.example.lab01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
