3c678b999af9e53bfdd772be521321b2e6be9a73
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2022-05-05 00:06:14 +0300

    Zhuk_Vladislav : Made 9 lab

---
+++ Zhuk_Vladislav/lab9/sources/app/src/main/java/com/example/lab9/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.lab9;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void toVideo(View view){
+        Intent intent = new Intent(this, video.class);
+        startActivity(intent);
+    }
+    public void toAudio(View view){
+        Intent intent = new Intent(this, Audio.class);
+        startActivity(intent);
+    }
+    public void toCamera(View view){
+        Intent intent = new Intent(this, Camera.class);
+        startActivity(intent);
+    }
+    public void toGallery(View view){
+        Intent intent = new Intent(this, gallery.class);
+        startActivity(intent);
+    }
+
+}
