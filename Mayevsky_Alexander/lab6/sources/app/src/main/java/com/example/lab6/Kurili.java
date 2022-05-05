5f3bd0a26a43b9a63fcdf6d72463b80f9ba3cf5e
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2022-05-05 14:38:42 +0300

    Mayevsky_Alexander : Made 6 lab

---
+++ Mayevsky_Alexander/lab6/sources/app/src/main/java/com/example/lab6/Kurili.java
@@ -0,0 +1,28 @@
+package com.example.lab6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Kurili extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kurili);
+    }
+    public void onClickBack(View view){
+        onBackPressed();
+    }
+    public void onClickImage(View view){
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.activity_dialog);
+        TextView text = (TextView) dialog.findViewById(R.id.dialogText);
+        text.setText(R.string.descriptionKu);
+        text.setTextSize(25);
+        dialog.show();
+    }
+}
