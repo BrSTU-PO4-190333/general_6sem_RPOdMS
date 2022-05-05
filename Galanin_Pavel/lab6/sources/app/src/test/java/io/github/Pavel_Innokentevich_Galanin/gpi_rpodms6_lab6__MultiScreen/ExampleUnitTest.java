2c0b98207807d79bcd94ba430a687d9a8279ebcf
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2022-05-05 07:59:37 +0300

    Galanin_Pavel : Made 6 lab

---
+++ Galanin_Pavel/lab6/sources/app/src/test/java/io/github/Pavel_Innokentevich_Galanin/gpi_rpodms6_lab6__MultiScreen/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package io.github.Pavel_Innokentevich_Galanin.gpi_rpodms6_lab6__MultiScreen;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
